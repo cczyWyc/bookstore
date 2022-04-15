25090cc3c349df174a9c35d1ab45a87a63924dea
cczyWyc <274230928@qq.com>
2022-04-16 00:08:49 +0800

    add book store

--- store/store.go
+++ store/store.go
@@ -1,1 +1,16 @@
 package store
+
+type Book struct {
+	Id      string   `json:"id"`
+	Name    string   `json:"name"`
+	Authors []string `json:"authors"`
+	Press   string   `json:"press"`
+}
+
+type Store interface {
+	Create(*Book) error       // create a new book item
+	Update(*Book) error       // update a book item
+	Get(string) (Book, error) //get a book item
+	GetAll() ([]Book, error)  //get all book message
+	Delete(string) error      //delete a book item
+}
